6a7d6181d45f66c24b94feb7ac4d99b08b122829
gbodeen <gabebodeen@gmail.com>
2018-12-07 17:53:15 -0600

    Use models.Sessions to communicate session info with db.

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,9 +1,31 @@
+var utils = require('../lib/hashUtils');
+
 const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  if (req.session) {
+    console.log('WE HIT THIS BRANCH \n \n');
+  } else {
+    models.Sessions.create()
+      .then(createResult => {
+        return models.Sessions.get({ 'id': createResult.insertId });
+      })
+      .then(getSession => {
+        req.session = getSession;
+        res.cookies.shortlyid = { value: getSession.hash };
+        req.session.userId = getSession.userId;
+        return models.Users.get({ 'id': getSession.userId });
+      })
+      .then(getUser => {
+        console.log('%%%%%%%%%%: getUser:  ', getUser);
+        req.session.user = { username: getUser ? getUser.username : 'NOBODY EVER AGAIN' };
+      })
+      .finally(() => {
+        next();
+      });
+  }
 };
-
 /************************************************************/
 // Add additional authentication middleware functions below
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,12 @@
 const parseCookies = (req, res, next) => {
+  if (req.headers.cookie) {
+    req.cookies = (JSON.parse('{' + req.headers.cookie.split('; ').map(pair => {
+      return '"' + pair.split('=').join('":"') + '"';
+    }).join(',') + '}'));
+  } else {
+    req.cookies = {};
+  }
+  next();
 };
 
 module.exports = parseCookies;
