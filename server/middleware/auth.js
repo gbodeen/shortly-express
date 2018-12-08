c988710bb874d157be68184a7c6125998287e10f
gbodeen <gabebodeen@gmail.com>
2018-12-07 19:18:55 -0600

    Pass spec 19 without cheating.

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -6,6 +6,21 @@ const Promise = require('bluebird');
 module.exports.createSession = (req, res, next) => {
   if (req.session) {
     console.log('WE HIT THIS BRANCH \n \n');
+  } else if (req.cookies.shortlyid) {
+    console.log('req.cookies.shortlyid      : ', req.cookies.shortlyid);
+    return models.Sessions.get({ 'hash': req.cookies.shortlyid })
+      .then(getSession => {
+        req.session = getSession;
+        req.session.userId = getSession.userId;
+        return models.Users.get({ 'id': getSession.userId });
+      })
+      .then(getUser => {
+        //console.log('%%%%%%%%%%: getUser:  ', getUser);
+        req.session.user = { username: getUser ? getUser.username : 'ANONYMOUS' };
+      })
+      .finally(() => {
+        next();
+      });
   } else {
     models.Sessions.create()
       .then(createResult => {
@@ -14,12 +29,6 @@ module.exports.createSession = (req, res, next) => {
       .then(getSession => {
         req.session = getSession;
         res.cookies.shortlyid = { value: getSession.hash };
-        req.session.userId = getSession.userId;
-        return models.Users.get({ 'id': getSession.userId });
-      })
-      .then(getUser => {
-        console.log('%%%%%%%%%%: getUser:  ', getUser);
-        req.session.user = { username: getUser ? getUser.username : 'NOBODY EVER AGAIN' };
       })
       .finally(() => {
         next();
