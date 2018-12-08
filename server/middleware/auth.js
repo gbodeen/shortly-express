235429aae728c6375776f46469e7776337bcca3e
gbodeen <gabebodeen@gmail.com>
2018-12-07 19:54:14 -0600

    Reset malicious cookies.

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,23 +4,32 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
-  if (req.session) {
-    console.log('WE HIT THIS BRANCH \n \n');
-  } else if (req.cookies.shortlyid) {
-    console.log('req.cookies.shortlyid      : ', req.cookies.shortlyid);
-    return models.Sessions.get({ 'hash': req.cookies.shortlyid })
+  if (req.session || req.cookies.shortlyid) {
+    models.Sessions.get({ 'hash': req.cookies.shortlyid })
       .then(getSession => {
-        req.session = getSession;
-        req.session.userId = getSession.userId;
-        return models.Users.get({ 'id': getSession.userId });
+        if (getSession) {
+          req.session = getSession;
+          req.session.userId = getSession.userId;
+          return models.Users.get({ 'id': getSession.userId });
+        } else {
+          return models.Sessions.create()
+            .then(createResult => {
+              return models.Sessions.get({ 'id': createResult.insertId });
+            })
+            .then(getSession => {
+              req.session = getSession;
+              res.cookies = { shortlyid: { value: getSession.hash } };
+              return models.Users.get({ 'id': getSession.userId });
+            });
+        }
       })
       .then(getUser => {
-        //console.log('%%%%%%%%%%: getUser:  ', getUser);
         req.session.user = { username: getUser ? getUser.username : 'ANONYMOUS' };
       })
       .finally(() => {
         next();
       });
+
   } else {
     models.Sessions.create()
       .then(createResult => {
@@ -28,7 +37,7 @@ module.exports.createSession = (req, res, next) => {
       })
       .then(getSession => {
         req.session = getSession;
-        res.cookies.shortlyid = { value: getSession.hash };
+        res.cookies = { shortlyid: { value: getSession.hash } };
       })
       .finally(() => {
         next();
